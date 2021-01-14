ac502f4c2425da82bdd8526793f835f04e08d704
Young-Mason <mason.yk3@gmail.com>
2021-01-14 13:39:32 +0900

    Update quiz

--- public/quizdb.js
+++ public/quizdb.js
@@ -1,25 +1,25 @@
 const quizdata = [
     {
         id: 1,
-        score: 16,
+        score: 20,
         question: '부산에 있는 TGIF 에서 만들어져 전세계에 퍼졌다고 알려져있으며 달콤한 향과 맛으로 유명한 칵테일은?',
         answers: [
             {
                 id: 1,
                 text: '준 벅',
-                isAnswer: false
+                isAnswer: true
             }
             ,
             {
                 id:2,
                 text: '존 박',
-                isAnswer: true
+                isAnswer: false
             }
         ]
     },
     {
         id: 2,
-        score: 16,
+        score: 20,
         question: '잭다니엘 위스키에 콜라를 섞어 마시는 매우 간단한 칵테일로, 제법 강한 도수와 거친 남성적인 이미지로 사랑받는 칵테일은?',
         answers: [
             {
@@ -41,7 +41,7 @@ const quizdata = [
     },
     {
         id: 3,
-        score: 16,
+        score: 20,
         question: '칵테일의 왕이라고도 불리며 007과 킹스맨 등 영화의 명대사로 유명한 칵테일은?',
         answers: [
             {
@@ -65,7 +65,61 @@ const quizdata = [
                 isAnswer: false
             }
         ]
-    }
+    },
+    {
+        id: 4,
+        score: 20,
+        question: '다음중 모히또의 재료가 아닌것은?',
+        answers: [
+            {
+                id: 1,
+                text: '럼',
+                isAnswer: false
+            },
+            {
+                id:2,
+                text: '라임',
+                isAnswer: false
+            },
+            {
+                id:3,
+                text: '몰디브',
+                isAnswer: true,
+            },
+            {
+                id:4,
+                text: '민트',
+                isAnswer: false
+            }
+        ]
+    },
+    {
+        id: 5,
+        score: 20,
+        question: '칵테일의 왕이라고도 불리며 007과 킹스맨 등 영화의 명대사로 유명한 칵테일은?',
+        answers: [
+            {
+                id: 1,
+                text: '하이볼',
+                isAnswer: true
+            },
+            {
+                id:2,
+                text: '하이바',
+                isAnswer: false
+            },
+            {
+                id:3,
+                text: '이하이',
+                isAnswer: false,
+            },
+            {
+                id:4,
+                text: '하이네켄',
+                isAnswer: false
+            }
+        ]
+    },
 ]
 
 export default quizdata
