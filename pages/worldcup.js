07abc3d363445c0621de7040be3c313c81a407ad
Doehyun Baek <doehyunbaek@gmail.com>
2021-01-19 16:59:44 +0900

    Finish carddealing

--- pages/worldcup.js
+++ pages/worldcup.js
@@ -5,22 +5,19 @@ import React, { useState, useReducer } from "react";
 import { useSprings, animated, interpolate } from "react-spring";
 import Button from "../components/Button";
 const Container = styled.div`
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
   background: lightblue;
   position: fixed;
-  overflow: hidden;
   width: 100%;
   height: 100%;
 `;
-const Outer = styled(animated.div)`
+const Card = styled(animated.div)`
   position: absolute;
   width: 100%;
   height: 100%;
   will-change: transform;
-  display: flex;
-  align-items: flex-start;
-  justify-content: center;
-`;
-const Inner = styled(animated.div)`
   background-color: white;
   background-size: auto 85%;
   background-repeat: no-repeat;
@@ -47,14 +44,10 @@ const to = (i) => ({
   x: 0,
   y: i * -4,
   scale: 1,
-  rot: -10 + Math.random() * 20,
+  rotate: -10 + Math.random() * 20,
   delay: i * 100,
 });
 const from = (i) => ({ x: 0, rot: 0, scale: 1.5, y: -1000 });
-const trans = (r, s) =>
-  `perspective(1500px) rotateX(30deg) rotateY(${
-    r / 10
-  }deg) rotateZ(${r}deg) scale(${s})`;
 const initialState = {
   deck: Array.from({ length: cards.length }, (_, i) => i),
   table: [],
@@ -66,22 +59,22 @@ function reducer(state, action) {
     case "deal":
       return {
         ...state,
-        deck: state.deck.slice(2),
-        table: state.deck.slice(0, 2),
+        deck: state.deck.slice(0, state.deck.length - 2),
+        table: state.deck.slice(state.deck.length - 2),
       };
     case "select_left":
       return {
         ...state,
-        deck: [...state.deck, state.table[0]],
+        deck: [state.table[0], ...state.deck],
         table: [],
-        leftGone: [...state.leftGone, state.table[0]],
+        rightGone: [...state.rightGone, state.table[1]],
       };
     case "select_right":
       return {
         ...state,
-        deck: [...state.deck, state.table[1]],
+        deck: [state.table[1], ...state.deck],
         table: [],
-        rightGone: [...state.rightGone, state.table[1]],
+        leftGone: [...state.leftGone, state.table[0]],
       };
     case "reset":
       return {
@@ -103,12 +96,29 @@ function Deck() {
   const [state, dispatch] = useReducer(reducer, initialState);
   React.useEffect(() => {
     set((i) => {
+      if (
+        state.deck.length === 1 &&
+        state.table.length === 0 &&
+        state.deck[0] == i
+      ) {
+        const x = 0;
+        const scale = 2;
+        const y = 200;
+        const rotate = 0;
+        return {
+          x,
+          y,
+          scale,
+          rotate,
+        };
+      }
       const isDeck = state.deck.includes(i);
       const isTableLeft = state.table.indexOf(i) === 0;
       const isTableRight = state.table.indexOf(i) === 1;
       const isLeftGone = state.leftGone.includes(i);
       const isRightGone = state.rightGone.includes(i);
       const w = window.innerWidth;
+      const scale = 1;
       const x = isDeck
         ? 0
         : isTableLeft
@@ -117,12 +127,16 @@ function Deck() {
         ? 200
         : isLeftGone
         ? -1000
-        : 1000;
+        : isRightGone
+        ? 1000
+        : 0;
       const y = isDeck ? 0 : 200;
+      const zIndex = state.deck.indexOf(i);
       return {
         x,
         y,
-        config: { friction: 50, tension: 500 },
+        zIndex,
+        scale,
       };
     });
   }, [state]);
@@ -140,19 +154,20 @@ function Deck() {
   return (
     <Container>
       <Button onClick={(e) => dispatch({ type: "reset" })}>Reset</Button>
-      {props.map(({ x, y, rot, scale }, i) => (
-        <Outer key={i} style={{ x, y }}>
-          <Inner
-            onClick={(e) => {
-              console.log(i);
-              handleClick(e, i);
-            }}
-            style={{
-              transform: interpolate([rot, scale], trans),
-              backgroundImage: `url(${cards[i]})`,
-            }}
-          />
-        </Outer>
+      {props.map(({ x, y, rotate, scale, zIndex }, i) => (
+        <Card
+          key={i}
+          onClick={(e) => {
+            handleClick(e, i);
+          }}
+          style={{
+            x,
+            y,
+            rotate,
+            scale,
+            backgroundImage: `url(${cards[i]})`,
+          }}
+        />
       ))}
     </Container>
   );
@@ -165,34 +180,3 @@ export default function WorldCup() {
     </PageUtils>
   );
 }
-
-// const bind = useGesture(
-//   ({
-//     args: [index],
-//     down,
-//     delta: [xDelta],
-//     distance,
-//     direction: [xDir],
-//     velocity,
-//   }) => {
-//     const trigger = velocity > 0.2; // If you flick hard enough it should trigger the card to fly out
-//     const dir = xDir < 0 ? -1 : 1; // Direction should either point left or right
-//     if (!down && trigger) gone.add(index); // If button/finger's up and trigger velocity is reached, we flag the card ready to fly out
-//     set((i) => {
-//       if (index !== i) return; // We're only interested in changing spring-data for the current spring
-//       const isGone = gone.has(index);
-//       const x = isGone ? (200 + window.innerWidth) * dir : down ? xDelta : 0; // When a card is gone it flys out left or right, otherwise goes back to zero
-//       const rot = xDelta / 100 + (isGone ? dir * 10 * velocity : 0); // How much the card tilts, flicking it harder makes it rotate faster
-//       const scale = down ? 1.1 : 1; // Active cards lift up a bit
-//       return {
-//         x,
-//         rot,
-//         scale,
-//         delay: undefined,
-//         config: { friction: 50, tension: down ? 800 : isGone ? 200 : 500 },
-//       };
-//     });
-//     if (!down && gone.size === cards.length)
-//       setTimeout(() => gone.clear() || set((i) => to(i)), 600);
-//   }
-// );
