36594159d4fd305e5e611a7c465ae025aaa87cea
Doehyun Baek <doehyunbaek@gmail.com>
2021-01-27 17:04:59 +0900

    Add image to circles

--- pages/circlepacking.js
+++ pages/circlepacking.js
@@ -1,4 +1,5 @@
 import * as d3 from "d3";
+import { useRouter } from "next/router";
 import { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 import Pageutils from "../components/Pageutils";
@@ -12,10 +13,10 @@ const color = d3
   .range(["hsl(152,80%,80%)", "hsl(228,30%,40%)"])
   .interpolate(d3.interpolateHcl);
 const pack = (data) =>
-  d3.pack().size([width, height]).padding(3)(
+  d3.pack().size([width, height]).padding(5)(
     d3
       .hierarchy(data)
-      .sum((d) => d.value)
+      .sum((d) => 100)
       .sort((a, b) => b.value - a.value)
   );
 
@@ -31,6 +32,7 @@ const SVG = styled.svg`
 `;
 
 export default function CirclePacking() {
+  const router = useRouter();
   const svgRef = useRef();
   useEffect(() => {
     let focus = root;
@@ -43,21 +45,25 @@ export default function CirclePacking() {
       .data(root.descendants().slice(1))
       .join("circle")
       .attr("fill", (d) => (d.children ? color(d.depth) : "white"))
-      .attr("pointer-events", (d) => (!d.children ? "none" : null))
       .on("mouseover", function () {
         d3.select(this).attr("stroke", "#000");
       })
       .on("mouseout", function () {
         d3.select(this).attr("stroke", null);
       })
-      .on(
-        "click",
-        (event, d) => focus !== d && (zoom(event, d), event.stopPropagation())
-      );
+      .on("click", (event, d) => {
+        if (focus !== d) {
+          if (d.data.value) {
+            setTimeout(() => router.push(`/cocktails/${d.data.value}`), 1000);
+          }
+          zoom(event, d);
+        }
+        event.stopPropagation();
+      });
 
     const label = svg
       .append("g")
-      .style("font", "10px sans-serif")
+      .style("font", "50px sans-serif")
       .attr("pointer-events", "none")
       .attr("text-anchor", "middle")
       .selectAll("text")
@@ -67,13 +73,29 @@ export default function CirclePacking() {
       .style("display", (d) => (d.parent === root ? "inline" : "none"))
       .text((d) => d.data.name);
 
+    const image = svg
+      .append("g")
+      .selectAll("image")
+      .data(root.descendants())
+      .join("image")
+      .attr("x", -40)
+      .attr("y", -40)
+      .attr("height", 75)
+      .attr("width", 75)
+      .attr("xlink:href", (d) =>
+        d.data.value ? `/cocktails/${d.data.value}.png` : ""
+      );
+
     zoomTo([root.x, root.y, root.r * 2]);
 
     function zoomTo(v) {
       const k = width / v[2];
 
       view = v;
-
+      image.attr(
+        "transform",
+        (d) => `translate(${(d.x - v[0]) * k},${(d.y - v[1]) * k})`
+      );
       label.attr(
         "transform",
         (d) => `translate(${(d.x - v[0]) * k},${(d.y - v[1]) * k})`
